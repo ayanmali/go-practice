5341bada76fdda7bb2f6ee6c33f74f85e4efcde2
Ayan Ali <ayan_ali2004@hotmail.com>
2024-08-30 17:56:39 -0400

    Added logic for determining correctness of a user answer

--- quiz/quiz.go
+++ quiz/quiz.go
@@ -1,10 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"encoding/csv"
 	"fmt"
 	"os"
-	"strconv"
+	"strings"
 )
 
 func main() {
@@ -19,23 +20,48 @@ func main() {
 	// Closing the file once the code is finished executing
 	defer file.Close()
 
-	reader := csv.NewReader(file)
-	records, err := reader.ReadAll()
+	csvReader := csv.NewReader(file)
+	records, err := csvReader.ReadAll()
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println("CSV file loaded")
 
 	// Creating the map to store questions and their answers
-	var questions map[string]int = make(map[string]int)
+	var questions map[string]string = make(map[string]string)
 
 	// Storing each question and its corresponding answer in the map
 	for _, record := range records {
-		intVal, err := strconv.Atoi(record[1])
-		if err == nil {
-			questions[record[0]] = intVal
-		}
+		answer := record[1]
+		questions[record[0]] = answer
+
 	}
 
-	//fmt.Println(questions)
+	// fmt.Println(questions)
+
+	// Creating the reader object
+	inputReader := bufio.NewReader(os.Stdin)
+
+	// Display each question and get user input for the answer
+	for key := range questions {
+		// Displays the given question
+		fmt.Printf("%s\n", key)
+		// Gets the users answer
+		userAnswer, _ := inputReader.ReadString('\n')
+		// Removing the newline character from the user answer string
+		//userAnswer = userAnswer[:len(userAnswer)-1]
+		userAnswer = strings.TrimSpace(userAnswer)
+
+		fmt.Printf("You answered %s\n", userAnswer)
+		fmt.Printf("The correct answer is %s\n", questions[key])
+
+		// Tells the user if they got it right or not
+		if userAnswer == questions[key] {
+			fmt.Println("Correct")
+		} else {
+			fmt.Println("Incorrect")
+		}
+		fmt.Println()
+	}
+
 }
