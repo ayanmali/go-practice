a4240ca77cfa2db4a9e1c37e4a6fab19faa97410
Ayan Ali <ayan.ali@peelregion.ca>
2024-08-30 16:11:04 -0400

    Added questions and answers to the map

--- quiz/quiz.go
+++ quiz/quiz.go
@@ -1,1 +1,41 @@
 package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	const filename string = "problems.csv"
+
+	// Opening the file
+	file, err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("File %v opened successfully", filename)
+	// Closing the file once the code is finished executing
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+	records, err := reader.ReadAll()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("CSV file loaded")
+
+	// Creating the map to store questions and their answers
+	var questions map[string]int = make(map[string]int)
+
+	// Storing each question and its corresponding answer in the map
+	for _, record := range records {
+		intVal, err := strconv.Atoi(record[1])
+		if err == nil {
+			questions[record[0]] = intVal
+		}
+	}
+
+	//fmt.Println(questions)
+}
