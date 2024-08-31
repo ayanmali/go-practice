af57930c7a0b38c3dead540e87149cc48f7d3041
Ayan Ali <ayan_ali2004@hotmail.com>
2024-08-31 08:23:41 -0400

    Added timer using goroutines and channels

--- quiz/quiz.go
+++ quiz/quiz.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
 )
 
 func main() {
@@ -34,34 +35,54 @@ func main() {
 	for _, record := range records {
 		answer := record[1]
 		questions[record[0]] = answer
-
 	}
 
 	// fmt.Println(questions)
 
 	// Creating the reader object
 	inputReader := bufio.NewReader(os.Stdin)
+	// initializing a counter variable to keep track of score
+	var correctAnswers int = 0
+	var total int = len(questions)
+	answerCh := make(chan string)
+	// Creating the timer
+	timer := time.NewTimer(30 * time.Second)
 
 	// Display each question and get user input for the answer
 	for key := range questions {
 		// Displays the given question
 		fmt.Printf("%s\n", key)
-		// Gets the users answer
-		userAnswer, _ := inputReader.ReadString('\n')
-		// Removing the newline character from the user answer string
-		//userAnswer = userAnswer[:len(userAnswer)-1]
-		userAnswer = strings.TrimSpace(userAnswer)
+		go func() {
+			// Gets the users answer
+			userAnswer, _ := inputReader.ReadString('\n')
+			// Removing the newline character from the user answer string
+			//userAnswer = userAnswer[:len(userAnswer)-1]
+			userAnswer = strings.TrimSpace(userAnswer)
 
-		fmt.Printf("You answered %s\n", userAnswer)
-		fmt.Printf("The correct answer is %s\n", questions[key])
+			fmt.Printf("You answered %s\n", userAnswer)
+			fmt.Printf("The correct answer is %s\n", questions[key])
 
-		// Tells the user if they got it right or not
-		if userAnswer == questions[key] {
-			fmt.Println("Correct")
-		} else {
-			fmt.Println("Incorrect")
+			answerCh <- userAnswer
+		}()
+
+		select {
+		case <-timer.C:
+			fmt.Println("Time is up")
+			fmt.Printf("Your final score is: %v / %v", correctAnswers, total)
+			return
+		case userAnswer := <-answerCh:
+			// Tells the user if they got it right or not
+			if userAnswer == questions[key] {
+				fmt.Println("Correct")
+				correctAnswers++
+			} else {
+				fmt.Println("Incorrect")
+			}
 		}
+
 		fmt.Println()
 	}
+
+	fmt.Printf("Your final score is: %v / %v", correctAnswers, total)
 
 }
